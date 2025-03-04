40b693da3e7313cbdfa5e8c1a7f366fca081fb8b
Ramon Noleto <devnoleto@gmail.com>
2025-03-04 18:29:05 -0300

    :tada: first commit + Input componente

---
+++ src/Form/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Input = ({ id, label, setValue, ...props }) => {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type="text"
+        name={id}
+        onChange={({ target }) => setValue(target.value)}
+        {...props}
+      />
+    </>
+  );
+};
+
+export default Input;
