485b34baef8112d1d61d368bc5e03c2365792159
Ramon Noleto <devnoleto@gmail.com>
2025-03-05 09:02:03 -0300

    Criação do componente de radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import Input from './Form/input';
-import Select from './Form/Select';
-
+import React from "react";
+import Input from "./Form/input";
+import Select from "./Form/Select";
+import Radio from "./Form/Radio";
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("");
 
   return (
     <form>
-      <Select options={['Smartphone', 'Tablet']} value={produto} setValue={setProduto} /> 
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} required/>
-      <Input id="email" label="Email" value={email} setValue={setEmail}/>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+      <Select
+        options={["Smartphone", "Tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
       <button>Enviar</button>
     </form>
-  )
-}
+  );
+};
 
-export default App
+export default App;
