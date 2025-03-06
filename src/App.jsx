001bde171b10ee0901160e2c4e0dc8a5d4bd99e4
Ramon Noleto <devnoleto@gmail.com>
2025-03-05 22:38:03 -0300

    Intalação do react-router-dom e teste de paginas teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,18 @@
-import React from "react";
-import Input from "./Form/input";
-import useForm from "./Hooks/useForm";
-import Slide from "./Slide";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './Home';
+import Sobre from './Sobre';
 
 const App = () => {
-  const cep = useForm('cep');
-  const email = useForm('email');
-  const nome = useForm();
-  const sobrenome = useForm(false);
-
-
-  function handleSubmit(event){
-    event.preventDefault();
-    if(cep.validate() && email.validate() && nome.validate()){
-      console.log('Enviou');
-    } else {
-      console.log('Não enviou');
-    }
-  }
-
-  const slides = [
-    {
-      id: 'slide1',
-      text: 'Slide 1'
-    },
-    {
-      id: 'slide2',
-      text: 'Slide 2'
-    },
-    {
-      id: 'slide3',
-      text: 'Slide 3'
-    }
-  ]
-
   return (
-    <Slide slides={slides} />
-    // <form onSubmit={handleSubmit}>
-    //   <Input
-    //     id="cep"
-    //     label="CEP"
-    //     type="text"
-    //     placeholder="00000-000"
-    //     {...cep}
-    //   />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='sobre' element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-    //   <Input
-    //     id="email"
-    //     label="Email"
-    //     type="email"
-    //     {...email}
-    //   />
-
-    //   <Input
-    //     id="nome"
-    //     label="Nome"
-    //     type="text"
-    //     {...nome}
-    //   />
-
-    //   <Input
-    //     id="sobrenome"
-    //     label="Sobrenome"
-    //     type="text"
-    //     {...sobrenome}
-    //   />
-    //   <button>Enviar</button>
-    // </form>
-  );
-};
-
-export default App;
+export default App
